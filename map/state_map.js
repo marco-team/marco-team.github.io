48b6257fa4de9c9c9b9687c411099094e04746ca
SarveshRamachandran <sarvesh.ramachandran5782@gmail.com>
2021-12-03 16:03:43 -0500

    Update state_map.js
    - Added append svg

--- map/state_map.js
+++ map/state_map.js
@@ -12,12 +12,14 @@
     
   // enter code to create svg
   const map_svg = d3.select("#main_content_wrap")
+  .append('svg')
   .attr("id", "choropleth")
   .attr("width", 960)
   .attr("height", 700)
   .attr("transform", "translate(" + 0 + "," + margin.top + ")")
   .on("dblclick", dblclicked);
   const info_svg = d3.select("#main_content_wrap")
+  .append('svg')
   .attr("id", "info")
   .attr("class", "info")
   .attr("width", 860)
